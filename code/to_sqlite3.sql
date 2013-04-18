31441e9fd63fd2d0bdb15eeeb33b6f135ed7ef91
Thomas Levine <occurrence@thomaslevine.com>
2013-04-17 21:55:46 -0400

    table numbers

--- code/to_sqlite3.sql
+++ code/to_sqlite3.sql
@@ -118,35 +118,34 @@ CREATE TABLE _t8 (
   "footnote"
 );
 
-
 .import '../data/lifetime_csv/table_1.csv' _t1
-DROP TABLE IF EXISTS t;
-ALTER TABLE _t1 RENAME TO t;
+DROP TABLE IF EXISTS t1;
+ALTER TABLE _t1 RENAME TO t1;
 
-.import '../data/lifetime_csv/table_2.csv' _t1
-DROP TABLE IF EXISTS t;
-ALTER TABLE _t2 RENAME TO t;
+.import '../data/lifetime_csv/table_2.csv' _t2
+DROP TABLE IF EXISTS t2;
+ALTER TABLE _t2 RENAME TO t2;
 
-.import '../data/lifetime_csv/table_3.csv' _t1
-DROP TABLE IF EXISTS t;
-ALTER TABLE _t3 RENAME TO t;
+.import '../data/lifetime_csv/table_3.csv' _t3
+DROP TABLE IF EXISTS t3a;
+ALTER TABLE _t3 RENAME TO t3a;
 
-.import '../data/lifetime_csv/table_4.csv' _t1
-DROP TABLE IF EXISTS t;
-ALTER TABLE _t4 RENAME TO t;
+.import '../data/lifetime_csv/table_4.csv' _t4
+DROP TABLE IF EXISTS t3b;
+ALTER TABLE _t4 RENAME TO t3b;
 
-.import '../data/lifetime_csv/table_5.csv' _t1
-DROP TABLE IF EXISTS t;
-ALTER TABLE _t5 RENAME TO t;
+.import '../data/lifetime_csv/table_5.csv' _t5
+DROP TABLE IF EXISTS t3c;
+ALTER TABLE _t5 RENAME TO t3c;
 
-.import '../data/lifetime_csv/table_6.csv' _t1
-DROP TABLE IF EXISTS t;
-ALTER TABLE _t6 RENAME TO t;
+.import '../data/lifetime_csv/table_6.csv' _t6
+DROP TABLE IF EXISTS t4;
+ALTER TABLE _t6 RENAME TO t4;
 
-.import '../data/lifetime_csv/table_7.csv' _t1
-DROP TABLE IF EXISTS t;
-ALTER TABLE _t7 RENAME TO t;
+.import '../data/lifetime_csv/table_7.csv' _t7
+DROP TABLE IF EXISTS t5;
+ALTER TABLE _t7 RENAME TO t5;
 
-.import '../data/lifetime_csv/table_8.csv' _t1
-DROP TABLE IF EXISTS t;
-ALTER TABLE _t8 RENAME TO t;
+.import '../data/lifetime_csv/table_8.csv' _t8
+DROP TABLE IF EXISTS t6;
+ALTER TABLE _t8 RENAME TO t6;
