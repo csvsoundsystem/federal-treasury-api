68d6ebacd6673508430bf15ba17781762f1e7ca9
Thomas Levine <occurrence@thomaslevine.com>
2013-04-17 21:40:15 -0400

    notes

--- code/to_sqlite3.sql
+++ code/to_sqlite3.sql
@@ -1,4 +1,23 @@
 -- Missing data is modeled as the empty string.
+==> ../data/lifetime_csv/table_1.csv <==
+CREATE TABLE _t2 (
+  "table" TEXT NOT NULL,
+  "date" TEXT NOT NULL,
+  "day" TEXT NOT NULL,
+  "account" TEXT NOT NULL,
+is_total,close_today,open_today,open_mo,open_fy,footnote
+  "type" TEXT NOT NULL,
+  "subtype" TEXT NOT NULL,
+  "item" TEXT NOT NULL,
+  "is_total" TEXT NOT NULL,
+  "today" FLOAT NOT NULL,
+  "mtd" FLOAT NOT NULL,
+  "fytd" FLOAT NOT NULL,
+  "footnote"
+);
+
+==> ../data/lifetime_csv/table_2.csv <==
+table,date,day,account,type,subtype,item,is_total,today,mtd,fytd,footnote
 CREATE TABLE _t2 (
   "table" TEXT NOT NULL,
   "date" TEXT NOT NULL,
@@ -13,7 +32,27 @@ CREATE TABLE _t2 (
   "fytd" FLOAT NOT NULL,
   "footnote"
 );
+
+==> ../data/lifetime_csv/table_3.csv <==
+table,date,day,account,type,subtype,item,is_total,today,mtd,fytd,footnote
+
+==> ../data/lifetime_csv/table_4.csv <==
+table,date,day,surtype,type,subtype,item,is_total,today,mtd,fytd,footnote
+
+==> ../data/lifetime_csv/table_5.csv <==
+table,date,day,surtype,type,subtype,item,is_total,today,mtd,fytd,footnote
+
+==> ../data/lifetime_csv/table_6.csv <==
+table,date,day,type,item,is_total,close_today,open_today,open_mo,open_fy,footnote
+
+==> ../data/lifetime_csv/table_7.csv <==
+table,date,day,type,classification,is_total,today,mtd,fytd,footnote
+
+==> ../data/lifetime_csv/table_8.csv <==
+table,date,day,type,classification,is_total,today,mtd,fytd,footnote
 .separator ','
+
+
 .import '../data/lifetime_csv/table_2.csv' _t2
 DROP TABLE IF EXISTS t2;
 ALTER TABLE _t2 RENAME TO t2;
