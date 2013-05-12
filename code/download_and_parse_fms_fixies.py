8c0718d95cc7188265b35a0874856e30d5d32b64
Burton DeWilde <burtondewilde@gmail.com>
2013-05-12 12:47:02 -0400

    tidied up download_and_parse script; much nicer

--- code/download_and_parse_fms_fixies.py
+++ code/download_and_parse_fms_fixies.py
@@ -25,32 +25,37 @@ os.system('mkdir -pv ' + FIXIE_DIR)
 os.system('mkdir -pv ' + DAILY_CSV_DIR)
 os.system('mkdir -pv ' + LIFETIME_CSV_DIR)
 
-# test for existence of fixies; if none, start from THE BEGINNING
-test_fixies = [f for f in os.listdir(FIXIE_DIR) if '.txt' in f]
+## DOWNLOAD! ##################################################################
+# test for existence of downloaded fixies
+test_fixies = sorted([f for f in os.listdir(FIXIE_DIR) if f.endswith('.txt')])
+# if none, start from THE BEGINNING
 if len(test_fixies) == 0:
 	start_date = datetime.date(2005, 6, 9)
+# else start from last available fixie date
 else:
-	start_date = datetime.date.today() - datetime.timedelta(days=7)
+	start_date = parse_fms_fixies_2.get_date_and_day(test_fixies[-1])[0]
+# always end with today
 end_date = datetime.date.today()
 
 # download all teh fixies!
 fnames = download_fms_fixies.download_fixies(start_date, end_date)
 
-# check all fixies against all parsed csvs
+# check all downloaded fixies against all parsed csvs
 downloaded_files = set([fixie.split('.')[0] for fixie in os.listdir(FIXIE_DIR)
-	if fixie != '.DS_Store'])
-def parsed_files():
-	return set([csv.split('_')[0] for csv in os.listdir(DAILY_CSV_DIR)])
+	if fixie.endswith('.txt')])
+parsed_files = set([csv.split('_')[0] for csv in os.listdir(DAILY_CSV_DIR)
+	if csv.endswith('.csv')])
 
-# fixies that have not been parsed into csvs
-new_files = list(downloaded_files.difference(parsed_files()))
-# PARSE THEM
+## PARSE! #####################################################################
+# fixies that have not yet been parsed into csvs
+new_files = sorted(list(downloaded_files.difference(parsed_files)))
+# parse all teh fixies!
 for f in new_files:
-	if len(f) == 0: continue
 	fname = os.path.join(FIXIE_DIR, f+'.txt')
 	print '\n', fname
 	dfs = parse_fms_fixies_2.parse_file(fname)
 
+	# each table for each date stored in separate csv files
 	for i, df in dfs.items():
 		daily_csv = os.path.join(DAILY_CSV_DIR, f.split('.')[0]+'_t'+str(i)+'.csv')
 		df.to_csv(daily_csv,
