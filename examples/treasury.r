6cd6f006f42101f97a97718131c6d900f8bc3e82
Thomas Levine <occurrence@thomaslevine.com>
2013-06-03 20:29:23 -0700

    finish the r example

--- examples/treasury.r
+++ examples/treasury.r
@@ -1,11 +1,20 @@
 library(plyr)
+library(utils)
 library(RJSONIO)
 
 treasury <- function(sql) {
-  url = 'https://box.scraperwiki.com/cc7znvq/47d80ae900e04f2/sql/'
-  query_string = urlencode({'q':sql})
-  ldply(
-    fromJSON(url + '?' + query_string),
-    function(row) {as.data.frame(t(row))}
-  )
+  url = paste('https://box.scraperwiki.com/cc7znvq/47d80ae900e04f2/sql/?q=', URLencode(sql), sep = '')
+  handle <- getCurlHandle()
+  body <- getURL(url, curl = handle)
+  if (200 == getCurlInfo(handle)$response.code) {
+    ldply(
+      fromJSON(body),
+      function(row) {as.data.frame(t(row))}
+    )
+  } else {
+    stop(body)
+  }
 }
+
+print(treasury('SELECT * FROM "t1" WHERE "date" = \'2013-05-22\';'))
+print(treasury('SELEC-nhaoesaoeuhasouesnaouhsaoe2013-05-22\';'))
