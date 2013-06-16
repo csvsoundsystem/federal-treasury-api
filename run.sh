8317fc131abc75de78f53782f645f951b2e7046c
Thomas Levine <occurrence@thomaslevine.com>
2013-06-16 11:34:50 -0700

    Check out gh-pages HEAD before running.

--- run.sh
+++ run.sh
@@ -6,6 +6,8 @@ if [ -d env ]; then
   echo Activated virtualenv
 fi
 
+git checkout gh-pages
+git pull origin gh-pages
 cd code
 ./download_and_parse_fms_fixies.py
 echo Downloaded and parsed fixies
