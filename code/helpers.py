f9c166572c3dc7e0f2987571a9041b0bb0720263
Thomas Levine <occurrence@thomaslevine.com>
2013-06-16 12:39:32 -0700

    the series comparer is a mess and doesn't work
    i'll fix it later

--- code/helpers.py
+++ code/helpers.py
@@ -12,20 +12,30 @@ NA_FILL = {
     numpy.dtype('float64'): 0,
     numpy.dtype('int64'): 0,
 }
+def fillna(series):
+    return series.fillna(NA_FILL[series.dtype])
 
 def check_parse(fixie_basename):
     observed = parse_file(os.path.join('fixtures', fixie_basename + '.txt'), 'r')
     expected = {i:pandas.read_csv(os.path.join('fixtures', '%s_t%d.csv' % (fixie_basename, i))) for i in range(1,9)}
     for table_number in expected.keys():
         expected[table_number]['date'] = expected[table_number]['date'].apply(lambda d: datetime.date(*map(int, d.split('-'))))
-        if 'close_today' in expected[table_number].columns:
-            print expected[table_number]['close_today']
-            print observed[table_number]['close_today']
 
     for table_number in expected.keys():
         for column_name in expected[table_number].columns:
             print column_name
+            observed_series = observed[table_number][column_name]
+            expected_series = expected[table_number][column_name]
+            observed_list = list(observed_series)
+            expected_list = list(expected_series)
+
+            if observed_series.name in {'open_today','open_mo','open_fy','close_today','today','mtd','fytd'}:
+                observed_list = map(int, observed_list)
+
             # Hack to deal with NAs, which are floats and annoying to compare.
-            observed_series = observed[table_number][column_name].fillna(NA_FILL[observed[table_number][column_name].dtype])
-            expected_series = expected[table_number][column_name].fillna(NA_FILL[observed[table_number][column_name].dtype])
-            n.assert_list_equal(*map(list, [observed_series, expected_series]))
+            observed_series = fillna(observed_series)
+            expected_series = fillna(expected_series)
+            observed_list = list(observed_series)
+            expected_list = list(expected_series)
+
+            n.assert_list_equal(observed_list, expected_list)
