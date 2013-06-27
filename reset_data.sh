4290af193475e425e65cf1e7da7e89c315480bd8
Brian Abelson <brianabelson@gmail.com>
2013-06-27 13:26:59 -0400

    updated reset_data.sh to work on virtual environment

--- reset_data.sh
+++ reset_data.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
+set -e
+
+if [ -d env ]; then
+  . ./env/bin/activate
+  echo Activated virtualenv
+fi
+
+git checkout master
+git pull origin master
+
 rm -r data/daily_csv/
 rm -r data/lifetime_csv/
 rm data/fms.db
+
 cd code
-python download_and_parse_fms_fixies.py
+./download_and_parse_fms_fixies.py
