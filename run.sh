31ffa310d160d08706cb53f49bc642546cd92a8b
Thomas Levine <occurrence@thomaslevine.com>
2013-06-28 19:57:54 -0700

    add description to run.sh for issue #65

--- run.sh
+++ run.sh
@@ -13,7 +13,13 @@ git pull origin master
   cd code
   python download_and_parse_fms_fixies.py
 )
-echo Downloaded and parsed fixies
+echo All of the fixies have been downloaded and parsed.
+echo
+echo Everything that the script created is in the data directory.
+echo The raw files are in data/fixie. They were parsed and converted
+echo to CSVs in the data/daily_csv directory. These are combined by
+echo table in the data/lifetime_csv directory.
+echo
 
 # Tweet if running on ScraperWiki
 if uname -n|grep scraperwiki; then
