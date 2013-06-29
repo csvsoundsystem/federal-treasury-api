5c5b417f64fb8dce25cf03dc52be06c5fcb7d6b2
Brian Abelson <brianabelson@gmail.com>
2013-06-29 18:57:02 +0000

    edited run.sh to not include welcome msg and to not tweet

--- run.sh
+++ run.sh
@@ -10,19 +10,7 @@ fi
 git checkout master
 git pull origin master
 (
-  cd code
+  cd ~/federal-treasury-api/code
   python download_and_parse_fms_fixies.py
 )
-echo All of the fixies have been downloaded and parsed.
-echo
-echo Everything that the script created is in the data directory.
-echo The raw files are in data/fixie. They were parsed and converted
-echo to CSVs in the data/daily_csv directory. These are combined by
-echo table in the data/lifetime_csv directory.
-echo
 
-# Tweet if running on ScraperWiki
-if uname -n|grep scraperwiki; then
-  echo Tweeting
-  python ~/twitter/tweetbot.py
-fi
