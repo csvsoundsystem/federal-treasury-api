60f7dc2550af74922e9303261939f77fbd49a343
Brian Abelson <brianabelson@gmail.com>
2013-06-29 22:38:10 -0400

    tunred is_it_running.py into a twitter message

--- tests/is_it_running.py
+++ tests/is_it_running.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python
 # Tested by Tom on Python 2.7.5 and Python 3.3.1 running on Arch Linux
-
 import json
 import datetime
 from requests import get
@@ -12,7 +11,7 @@ def date_pair(date_date):
         'date': date_date.strftime('%A, %B %d, %Y'),
     }
 
-def observed():
+def observed_data():
     url = 'https://premium.scraperwiki.com/cc7znvq/47d80ae900e04f2/sql'
     sql = '''SELECT max(date) FROM t1;'''
 
@@ -22,7 +21,7 @@ def observed():
 
     return date_pair(date_date)
 
-def expected():
+def expected_data():
     'The date when the script should have last run'
     adate = datetime.date.today()
     adate -= datetime.timedelta(days=1)
@@ -31,15 +30,29 @@ def expected():
     return date_pair(adate)
 
 # Error if it hasn't run in half a week.
-def produce_message():
-    PEEPS = "@brianabelson @mhkeller @jbialer @thomaslevine @bdewilde @Cezary"
-    observed = observed()
-    expected = expected()
+def gen_message(tweet=True):
+    peeps = "@brianabelson @mhkeller @jbialer @thomaslevine @bdewilde @Cezary"
+    current_date = datetime.datetime.now().strftime("%Y-%m-%d %H-%M-%S")
+
+    observed = observed_data()
+    expected = expected_data()
+
     if observed['days'] > 7:
-        return twt =  "%s - something is definitely wrong at %s" % (PEEPS, datetime.datetime.now().strftime("%Y-%m-%d %H-%M-%S"))
+        if tweet:
+            return "Yo %s! Something is definitely wrong! - %s" % (peeps, current_date)
+        else:
+            return "The parser last ran on %s. Something is definitely wrong!" % observed['date']
 
     elif observed['date'] < expected['date']:
-        print("If %(date)s;  that's not a holiday, something is wrong." % expected)
-        exit(2)
+        if tweet:
+            return "Hey %s, somethings wrong if %s is a holiday! - %s" % (peeps, expected['date'])
+        else:
+            return "Unless %s is a holiday, something is up!" % expected['date']
     else:
-        print('All seems well.')
+        if tweet:
+            return None
+        else:
+            return "All seems well!"
+
+if __name__ == '__main__':
+    print gen_message(tweet=False)
