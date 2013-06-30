3752147a03638d0ae2179a74bc163b5163b48a85
Brian Abelson <brianabelson@gmail.com>
2013-06-30 14:44:32 -0400

    changes to crontab

--- utils/crontab.sh
+++ utils/crontab.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 echo "0 17 * * * cd '$PWD' && git pull && ./run.sh"
-echo "0 12 * * * cd '$PWD' && /usr/bin/python tests/is_it_running.py --tweet True
+echo "0 12 * * * cd '$PWD' && /usr/bin/python twitter/tweetbot.y -t is_it_running
