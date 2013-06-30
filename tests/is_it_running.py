41b8512fce94765503acaff0e87a27931ca3d4fc
Brian Abelson <brianabelson@gmail.com>
2013-06-30 01:06:00 -0400

    is_it_running.py now issues a tweet if somethings wrong, it also functions normally(not tweeting) if you run it without the "--tweet True" flag

--- tests/is_it_running.py
+++ tests/is_it_running.py
@@ -2,13 +2,15 @@
 # Tested by Tom on Python 2.7.5 and Python 3.3.1 running on Arch Linux
 import json
 import datetime
+import treasuryio
+import tweepy
 from requests import get
 from optparse import OptionParser
 
 def load_options():
     parser = OptionParser()
     parser.add_option("-t", "--tweet", dest="tweet", default=False,
-                  help="Whether or not to tweet results for test", metavar="FILE")
+                  help="Whether or not to tweet results for test")
     (options, args) = parser.parse_args()
     return options
 
@@ -36,47 +38,38 @@ def expected_data():
         adate -= datetime.timedelta(days=1)
     return date_pair(adate)
 
-@treasuryio.tweet
 def gen_test_message():
-    peeps = "@brianabelson @mhkeller @jbialer @thomaslevine @bdewilde @Cezary"
-    current_date = datetime.datetime.now().strftime("%Y-%m-%d %H-%M-%S")
-
     observed = observed_data()
     expected = expected_data()
 
     if observed['days'] > 7:
-        return "The parser last ran on %s. Something is definitely wrong!" % observed['date']
-
+        print "The parser last ran on %s. Something is definitely wrong!" % observed['date']
     elif observed['date'] < expected['date']:
-        return "Unless %s is a holiday, something is up!" % expected['date']
+        print "Unless %s is a holiday, something is up!" % expected['date']
     else:
-        return "All seems well!"
+        print "All seems well!"
 
 @treasuryio.tweet
-def gen_test_tweet(tweet=True):
+def gen_test_tweet():
     peeps = "@brianabelson @mhkeller @jbialer @thomaslevine @bdewilde @Cezary"
     current_date = datetime.datetime.now().strftime("%Y-%m-%d %H-%M-%S")
 
     observed = observed_data()
     expected = expected_data()
 
-    if observed['days'] > 7:
-        if tweet:
-            return "Yo %s! Something is definitely wrong! - %s" % (peeps, current_date)
-        else:
-            return "The parser last ran on %s. Something is definitely wrong!" % observed['date']
-
+    if observed['days'] > 5:
+        return "Yo %s! Something is definitely wrong! - %s" % (peeps, current_date)
     elif observed['date'] < expected['date']:
-        if tweet:
-            return "Hey %s, somethings wrong if %s is a holiday! - %s" % (peeps, expected['date'])
-        else:
-            return "Unless %s is a holiday, something is up!" % expected['date']
+        return "Hey %s, somethings wrong unless %s is a holiday! - %s" % (peeps, expected['date'])
     else:
-        if tweet:
-            return None
-        else:
-            return "All seems well!"
+        return None
 
 if __name__ == '__main__':
     options = load_options()
-    print gen_message(tweet=False)
+    if options.tweet:
+        try:
+            gen_test_tweet()
+        except tweepy.error.TweepError:
+            pass
+    else:
+        gen_test_message()
--- utils/crontab.sh
+++ utils/crontab.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
 echo "0 17 * * * cd '$PWD' && git pull && ./run.sh"
+echo "0 12 * * * cd '$PWD' && /usr/bin/python tests/is_it_running.py --tweet True
