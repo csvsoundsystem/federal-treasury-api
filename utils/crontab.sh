a32af40d0e66cb3b1c8cbd88b07fee54275b5e25
Thomas Levine <occurrence@thomaslevine.com>
2013-07-01 01:10:45 -0700

    switch crontab mailto for ./monitor.sh

--- utils/crontab.sh
+++ utils/crontab.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
-MAILTO="csvsoundsystem@gmail.com"
-echo "0 17 * * * cd '$PWD' && git pull && ./run.sh"
+echo "0 17 * * * cd '$PWD' && git pull && ./monitor.sh ./run.sh"
 echo "52 14 * * * cd '$PWD' && /usr/bin/python twitter/tweetbot.y -t is_it_running"
