df7f98b924d200b4c8b9eb09abaf335b475d4f24
Brian Abelson <brianabelson@gmail.com>
2013-07-03 10:54:38 -0400

    cleaned the code up a bit

--- parser/parse_fms_fixies.py
+++ parser/parse_fms_fixies.py
@@ -4,6 +4,7 @@ import datetime
 import pandas as pd
 import re
 
+# Global Vars
 NORMALIZE_FIELD_TABLE = json.load(open("../parser/normalize_field_table.json"))
 
 T4_USE_ITEMS = [
@@ -13,6 +14,7 @@ T4_USE_ITEMS = [
 	'Federal Reserve Account Total',
 	'Federal Reserve Account Depositaries'
 ]
+
 ################################################################################
 def normalize_fields(text, table, field):
 	table_lookup = NORMALIZE_FIELD_TABLE[table]
@@ -148,12 +150,12 @@ def parse_table(table, date, verbose=False):
 		# what's our line number? shall we bail out?
 		index += 1
 		if index <= used_index: continue
-
 		indent = len(re.search(r'^\s*', line).group())
 
 		# Rows that we definitely want to skip
 		# empty or centered header rows
 		if re.match(r'^\s{7,}', line): continue
+
 		# page number rows
 		page_number_match = re.search(r'\d+.*DAILY\s+TREASURY\s+STATEMENT.*PAGE:\s+(\d+)', line)
 		if page_number_match:
@@ -166,40 +168,39 @@ def parse_table(table, date, verbose=False):
 		if re.search(r'DAILY\s+TREASURY\s+STATEMENT', line):
 			continue
 		# comment on statutory debt limit at end of Table III-C, and beyond
-		if re.search(r'(As|Act) of ([A-Z]\w+ \d+, \d+|\d+\/\d+\/\d+)', line) and re.search(r'(statutory )*debt( limit)*', line):
+		elif re.search(r'(As|Act) of ([A-Z]\w+ \d+, \d+|\d+\/\d+\/\d+)', line) and re.search(r'(statutory )*debt( limit)*', line):
 			break
 		# comment on whatever this is; above line may make this redundant
-		if re.search(r'\s*Unamortized Discount represents|amortization is calculated daily', line, flags=re.IGNORECASE):
+		elif re.search(r'\s*Unamortized Discount represents|amortization is calculated daily', line, flags=re.IGNORECASE):
 			break
 		# more cruft of a similar sort
-		if re.search(r'billion after \d+\/\d+\/\d+', line):
+		elif re.search(r'billion after \d+\/\d+\/\d+', line):
 			continue
 		# comment about food stamp program euphemism
-		if re.search(r'\s*The Food Stamp Program has been renamed', line, flags=re.IGNORECASE):
+		elif re.search(r'\s*The Food Stamp Program has been renamed', line, flags=re.IGNORECASE):
 			break
 		# final footer of file
-		if re.search(r"\s+This statement summarizes\s+the United States Treasury's cash and debt", line):
+		elif re.search(r"\s+This statement summarizes\s+the United States Treasury's cash and debt", line):
 			break
 		# final footer of file -- above line should make this redundant! but just in case
-		if re.search(r'\s+SOURCE:\s+Financial\s+Management', line):
+		elif re.search(r'\s+SOURCE:\s+Financial\s+Management', line):
 			break
-		if re.search(r'.*were no longer reported as.*', line):
+		elif re.search(r'.*were no longer reported as.*', line):
 			break
-		if re.search(r'.*leaving a portion of the funds.*', line):
+		elif re.search(r'.*leaving a portion of the funds.*', line):
 			break
-		if re.search(r'.*not include million offset .*', line, re.IGNORECASE):
+		elif re.search(r'.*not include million offset .*', line, re.IGNORECASE):
 			break
-		if re.search(r'.*began a pilot program for the repurchase.*', line, re.IGNORECASE):
+		elif re.search(r'.*began a pilot program for the repurchase.*', line, re.IGNORECASE):
 			break
-		if re.search(r'.*and million for the fiscal year to date for.*', line, re.IGNORECASE):
+		elif re.search(r'.*and million for the fiscal year to date for.*', line, re.IGNORECASE):
 			break
-		if re.search(r'.*dated December.*', line, re.IGNORECASE):
+		elif re.search(r'.*dated December.*', line, re.IGNORECASE):
 			break
-		if re.search(r'.*Treasury reduced the amount of Debt Subject.*', line, re.IGNORECASE):
+		elif re.search(r'.*Treasury reduced the amount of Debt Subject.*', line, re.IGNORECASE):
 			break
-		if re.search(r'.*very Act of Public Law ( ) These long term nonmarketable.*', line, re.IGNORECASE):
+		elif re.search(r'.*very Act of Public Law ( ) These long term nonmarketable.*', line, re.IGNORECASE):
 			break
-
 
 		# skip table header rows
 		if get_table_name(line):
@@ -224,16 +225,21 @@ def parse_table(table, date, verbose=False):
 					footnote[1] = ''.join([footnote[1], next_line])
 					used_index = index + i
 					i += 1
+
 			# make our merged footnote hack official!
 			footnotes[footnote[0]] = footnote[1]
+
 			# if next line after footnote is not another footnote
 			# it is most assuredly extra comments we don't need
 			try:
 				last_line = table[index + i]
+
 			except IndexError:
 				break
+
 			if not get_footnote(last_line):
 				break
+
 			# JUST IN CASE?! BJD: figure out why this is needed...
 			continue
 
@@ -244,9 +250,11 @@ def parse_table(table, date, verbose=False):
 		# separate digits and words
 		digits = re.findall(r'(-{,1}\d+)', line)
 		words = re.findall(r'\(\-\)|[()]|[^\W\d]+:?', line)
+
 		# check for (-) in words => multiply all digits by -1
 		if '(-)' in words:
 			digits = [str((-1)*int(digit)) for digit in digits]
+
 		# bug fix, to remove the govt's usage of 'r/' in front of numbers
 		# to denote revised values
 		text = ' '.join(word for word in words if word not in ['r', '(-)'])
@@ -257,8 +265,10 @@ def parse_table(table, date, verbose=False):
 			type_indent = indent
 			type_index = index
 			continue
+
 		elif indent <= type_indent:
 			type_ = None
+
 		row['type'] = type_
 
 		# get subtype row
@@ -267,37 +277,52 @@ def parse_table(table, date, verbose=False):
 			subtype_indent = indent
 			subtype_index = index
 			continue
+
 		if index == subtype_index + 1: pass # maybe unnecessary
+
 		elif indent <= subtype_indent:
 			subtype = None
+
 		row['subtype'] = subtype
 
 		# get and merge two-line rows
 		if len(digits) == 0 and not text.endswith(':'):
+
 			if two_line_delta == 1 or not re.search(r'TABLE I\s', row.get('table', '')):
+
 				try:
 					next_line = table[index + 1]
+
 					# check for footnotes, then note and erase them if present!
 					if re.search(r'\d+\/', next_line):
 						row['footnote'] = re.search(r'(\d+)\/', next_line).group(1)
 						next_line = re.sub(r'\d+\/', '', next_line)
+
 					next_digits = re.findall(r'(\d+)', next_line)
 					next_words = re.findall(r'[^\W\d]+:?', next_line)
+
 					if len(next_digits) != 0:
 						text = text + ' ' + ' '.join(next_words)
 						digits = next_digits
 						used_index = index + 1
-				except IndexError: pass
+
+				except IndexError:
+					pass
+
 			elif two_line_delta == -1 and re.search(r'TABLE I\s', row.get('table', '')):
+
 				try:
 					prev_line = table[index - 1]
 					prev_digits = re.findall(r'(\d+)', prev_line)
 					prev_words = re.findall(r'[^\W\d]+:?', prev_line)
+
 					if len(prev_digits) != 0:
 						text = ' '.join(prev_words) + ' ' + text
 						digits = prev_digits
 						get_rid_of_prev_line = parsed_table.pop()
-				except IndexError: pass
+
+				except IndexError:
+					pass
 
 
 		#print len(digits), '||', digits
@@ -305,6 +330,7 @@ def parse_table(table, date, verbose=False):
 		# this is a band-aid at best, sorry folks
 		if len(digits) == 0:
 			continue
+
 		if len(text) > 80:
 			continue
 
@@ -313,6 +339,7 @@ def parse_table(table, date, verbose=False):
 		# parse one table at a time
 
 		if re.search(r'TABLE I\s', row.get('table', '')):
+
 			try:
 				row['account_raw'] = text
 				row['account'] = normalize_fields(text, 't1', 'account')
@@ -320,11 +347,13 @@ def parse_table(table, date, verbose=False):
 				row['open_today'] = digits[-3]
 				row['open_mo'] = digits[-2]
 				row['open_fy'] = digits[-1]
+
 			except:
 				if verbose is True:
 					print 'WARNING:', line
 
 		elif re.search(r'TABLE II\s', row.get('table', '')):
+
 			try:
 				row['item_raw'] = text
 				row['item'] = normalize_fields(text, 't2', 'item')
@@ -335,8 +364,10 @@ def parse_table(table, date, verbose=False):
 				row['account'] = row['type']
 				# this is a hack, deal with it :-/
 				row['type'] = 'deposit'
+
 				if int(page_number) == 3:
 					row['type'] = 'withdrawal'
+
 				if row.get('subtype'):
 					subtype = row['subtype']
 					row['subitem'] = row['item']
@@ -344,17 +375,20 @@ def parse_table(table, date, verbose=False):
 					row['item'] = subtype
 					row.pop('subtype')
 					row.pop('item_raw')
+
 			except:
 				if verbose is True:
 					print 'WARNING:', line
 
 		elif re.search(r'TABLE III-A', row.get('table', '')):
+
 			try:
 				row['item_raw'] = text
 				row['item'] = normalize_fields(text, "t3a", 'item')
 				row['today'] = digits[-3]
 				row['mtd'] = digits[-2]
 				row['fytd'] = digits[-1]
+
 				if row.get('subtype'):
 					subtype = row['subtype']
 					row['subitem'] = row['item']
@@ -362,17 +396,20 @@ def parse_table(table, date, verbose=False):
 					row['item'] = subtype
 					row.pop('subtype')
 					row.pop('item_raw')
+
 			except:
 				if verbose is True:
 					print 'WARNING:', line
 
 		elif re.search(r'TABLE III-B', row.get('table', '')):
+
 			try:
 				row['item_raw'] = text
 				row['item'] = normalize_fields(text, "t3b", 'item')
 				row['today'] = digits[-3]
 				row['mtd'] = digits[-2]
 				row['fytd'] = digits[-1]
+
 				if row.get('subtype'):
 					subtype = row['subtype']
 					row['subitem'] = row['item']
@@ -380,11 +417,13 @@ def parse_table(table, date, verbose=False):
 					row['item'] = subtype
 					row.pop('subtype')
 					row.pop('item_raw')
+
 			except:
 				if verbose is True:
 					print 'WARNING:', line
 
 		elif re.search(r'TABLE III-C', row.get('table', '')):
+
 			try:
 				row['item_raw'] = text
 				row['item'] = normalize_fields(text, 't3c', 'item')
@@ -392,11 +431,13 @@ def parse_table(table, date, verbose=False):
 				row['open_today'] = digits[-3]
 				row['open_mo'] = digits[-2]
 				row['open_fy'] = digits[-1]
+
 			except:
 				if verbose is True:
 					print 'WARNING:', line
 
 		elif re.search(r'TABLE IV', row.get('table', '')):
+
 			try:
 				row['type'] = ''
 				row['classification_raw'] = text
@@ -418,17 +459,20 @@ def parse_table(table, date, verbose=False):
 					row['type'] = "source"
 				else:
 					row['type'] = "use"
+
 			except:
 				if verbose is True:
 					print 'WARNING:', line
 
 		elif re.search(r'TABLE V\s', row.get('table', '')):
+
 			try:
 				row['balance_transactions'] = text
 				row['depositary_type_a'] = digits[-4]
 				row['depositary_type_b'] = digits[-3]
 				row['depositary_type_c'] = digits[-2]
 				row['total'] = digits[-1]
+
 			except:
 				if verbose is True:
 					print 'WARNING:', line
@@ -442,8 +486,10 @@ def parse_table(table, date, verbose=False):
 				row['fytd'] = digits[-1]
 				if '( eft )' in row.get('classification_raw', '').lower():
 					row['type'] = 'EFT'
+
 				elif '( checks )' in row.get('classification_raw', '').lower():
 					row['type'] = 'CHECKS'
+
 			except:
 				if verbose is True:
 					print 'WARNING:', line
