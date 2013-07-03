1e46e0c2c0fa989e42b52dc23307f1a1575f5eee
Brian Abelson <brianabelson@gmail.com>
2013-07-02 21:04:48 -0400

    simplified tweetbot

--- tweet.sh
+++ tweet.sh
@@ -4,13 +4,7 @@ set -e
 if [ -d env ]; then
   . ./env/bin/activate
   echo Activated virtualenv
-  pip install --upgrade -r requirements.pip
 fi
 
-git checkout master
-git pull origin master
-
-(
-  cd ./twitter
-  python tweetbot.py $1
-)
+cd ./twitter
+python tweetbot.py $1
