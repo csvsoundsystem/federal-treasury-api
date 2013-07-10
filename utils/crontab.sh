6c23383547acf50d1fc66d9b805cb98da0a12e10
Brian Abelson <brianabelson@gmail.com>
2013-07-10 10:01:14 -0400

    removed monitor.sh from crontab

--- utils/crontab.sh
+++ utils/crontab.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 MAILTO="csvsoundsystem@gmail.com"
-30 16 * * * cd '$PWD' && git pull && ./monitor.sh && ./run.sh
+30 16 * * * cd /home && git pull && ./run.sh
 *  17 * * 1,4  /bin/bash /home/tweet.sh change_in_balance
 10 15 * * 2,5  /bin/bash /home/tweet.sh total_debt
 30 11 * * * /bin/bash /home/tweet.sh is_it_running
