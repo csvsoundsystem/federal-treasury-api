cf676c40fe46985cf06283ef5af46bcfb5ddd319
Thomas Levine <occurrence@thomaslevine.com>
2013-07-10 10:15:01 -0700

    install_crontab.sh

--- utils/install_crontab.sh
+++ utils/install_crontab.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 crontab -l > /tmp/crontab.bak
-cat "$HOME/crontab" | crontab
+cat "$HOME/utils/crontab" | crontab
 echo "NEW CRONTAB:"
 crontab -l
 echo "OLD CRONTAB: Backed up to /tmp/crontab.bak"
