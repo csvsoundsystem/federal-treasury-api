6920123d7eca5fbe8ec69dad656f986ef6aff95e
Thomas Levine <occurrence@thomaslevine.com>
2013-07-21 00:06:40 -0700

    check new mail and mark as read

--- code/email.py
+++ code/email.py
@@ -5,18 +5,24 @@ def login():
     M.login(os.environ['IMAP_USER'], os.environ['IMAP_PASSWORD'])
     return M
 
-def previous_email(search_string = '(FROM "fms.treas.gov")'):
+def new_mail(search_string = '(UNSEEN FROM "fms.treas.gov")'):
     M = login()
     M.select()
 
     # List the emails
-    status, header = M.search(None, search_string)
-    num = header[0].split()[-1]
+    status, numbers = M.search(None, search_string)
 
-    # Source of the most recent one.
-    status, eml = M.fetch(num, '(RFC822)')
+    # Check for new mail
+    _new_mail = numbers != ['']
 
+    # Mark new mail as read.
+    if _new_mail:
+        for number in numbers[0].split():
+            M.fetch(number, '(RFC822)')
+
+    # Log out
     M.close()
     M.logout()
 
-    return eml[0][1]
+    # Return True or False
+    return _new_mail
