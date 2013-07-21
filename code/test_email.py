fe7e57cfb1e725e3c6b87b2673123d031ee08f2f
Thomas Levine <occurrence@thomaslevine.com>
2013-07-21 00:13:31 -0700

    test new_mail

--- code/test_email.py
+++ code/test_email.py
@@ -3,3 +3,14 @@ import email
 def test_login():
     'I should be able to log in.'
     email.login()
+
+def test_new_mail():
+    _unseen ='(UNSEEN FROM "occurrence@thomaslevine.com" SUBJECT "Test email number 1")'
+    _all ='(FROM "occurrence@thomaslevine.com" SUBJECT "Test email number 1")'
+
+    M = email.login()
+    M.select()
+
+    M.store('2', '-FLAGS', '(\Seen)')
+    assert email.new_mail(search_string = _unseen)
+
