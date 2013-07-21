c8cb4d60114d40bbefd3b34c04c710e44d4113ab
Thomas Levine <occurrence@thomaslevine.com>
2013-07-20 23:59:04 -0700

    check the most recent fms email

--- code/email.py
+++ code/email.py
@@ -5,14 +5,17 @@ def login():
     M.login(os.environ['IMAP_USER'], os.environ['IMAP_PASSWORD'])
     return M
 
-def main():
+def previous_email(search_string = '(FROM "fms.treas.gov")'):
     M = login()
     M.select()
-    typ, data = M.search(None, '(UNSEEN FROM "fms.treas.gov")')
-    for num in data[0].split():
-        typ, data = M.fetch(num, '(RFC822)')
-        print 'Message %s\n%s\n' % (num, data[0][1])
+
+    # List the emails
+    status, header = M.search(None, search_string)
+    num = header[0].split()[-1]
+
+    # Read the most recent one.
+    status, eml = M.fetch(num, '(RFC822)')
+    print eml[0][1]
+
     M.close()
     M.logout()
-
-main()
