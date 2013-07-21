6bede47f8b4323e33cbdc108b96d3757a7a68bad
Thomas Levine <occurrence@thomaslevine.com>
2013-07-21 00:17:58 -0700

    test new_mail more completely

--- code/test_email.py
+++ code/test_email.py
@@ -11,6 +11,17 @@ def test_new_mail():
     M = email.login()
     M.select()
 
+    # Let's make sure that the fixtures are working;
+    # we should only have one email called "Test email number 1".
+    assert M.search(None, _all)[1] == ['2']
+
     M.store('2', '-FLAGS', '(\Seen)')
+
+    # The email should be new now.
     assert email.new_mail(search_string = _unseen)
 
+    # The email should not be new; we just saw it.
+    assert not email.new_mail(search_string = _unseen)
+
+    # Let's check that the email has been seen, just to be sure.
+    assert M.search(None, _unseen)[1] == ['']
