3bed786434ad5e52f4288edad99511a240fd2f55
Brian Abelson <brianabelson@gmail.com>
2013-07-23 14:38:46 -0400

    why isnt the crontab working

--- utils/install_crontab.sh
+++ utils/install_crontab.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 crontab -l > /tmp/crontab.bak
-cat "$HOME/utils/crontab" | crontab
+cat crontab.sh | crontab
 echo "NEW CRONTAB:"
 crontab -l
 echo "OLD CRONTAB: Backed up to /tmp/crontab.bak"
