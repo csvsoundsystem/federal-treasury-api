e5f1731008a1df130b02dc6b4aa931d523e93454
Brian Abelson <brianabelson@gmail.com>
2013-07-23 20:23:26 -0400

    getting loggin working

--- utils/crontab.sh
+++ utils/crontab.sh
@@ -6,5 +6,5 @@ MAILTO="csvsoundsystem+treasuryiocrontab@gmail.com"
 45 00,02,13,17 * * * cd /home && sh tweet.sh random_item >> logs/twt_log 2>> logs/twt_err.log
 30 11,15,22 * * * cd /home && sh tweet.sh random_comparison >> logs/twt_log 2>> logs/twt_err.log
 01 8 * * * cd /home/utils/ && sh reset_data.sh >> ../logs/run_log 2>> ../logs/run_err.log
-30 16 * * * cd /home/tests/ && python null_tests.py && python is_it_running.py  >> ../logs/test_log 2>> ../logs/test_err.log
+30 16 * * * cd /home/tests/ && python null_tests.py && python is_it_running.py  >> ../logs/test.log 2>> ../logs/test_err.log
 
